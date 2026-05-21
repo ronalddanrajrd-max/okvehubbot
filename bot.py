76b3dc6a8e93a86c23b1c19de043fa1dc87f5629
ronalddanrajrd-max <ronald.danraj.rd@gmail.com>
2026-05-21 15:02:26 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté : {bot.user}")
+    await bot.tree.sync()
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
