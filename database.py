c067ab07a9edc87a2d44c38b15999e506ac04893
ronalddanrajrd-max <ronald.danraj.rd@gmail.com>
2026-05-21 16:35:15 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
